716a314db2603ce608d9d11bb01b4af424011677
Katerina Plushko <katplush@yahoo.com>
2019-05-25 00:10:30 -0700

    completed todos and ran build

--- src/common/api.js
+++ src/common/api.js
@@ -9,8 +9,7 @@ export const API = axios.create({
 API.interceptors.request.use(function (config) {
     // Set common parameters for each request to the API.
 
-    // TODO: Use your own APPID for OpenWeatherMap.org
-    config.params.APPID = 'YOUR_APPID_HERE';
+    config.params.APPID = '929563d37fd0452ebae9f421022a2a3d';
     config.params.units = 'imperial';
 
     return config;
